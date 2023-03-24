082f0a9dba6188450243ce6f7708ea1758d04701
Francisco_Melendez <francisco.melendez14454@est.uca.edu.ni>
2023-03-24 01:34:18 -0600

    Agregue listarEstudiantes y agregarEstudiantes (Falta)

--- Conexion.py
+++ Conexion.py
@@ -1,6 +1,7 @@
 import pymysql.cursors
 
 class Conexion:
+    #recolecta los datos de la BD
     _DATABASE = 'Prueba_Estudiantes'
     _USERNAME = 'francisco'
     _PASSWORD = '1234'
@@ -9,7 +10,7 @@ class Conexion:
     _cursor = None
 
     @classmethod
-    def AgregarEstudiante(cls):
+    def obtenerConexion(cls):
         try:
             cls._conexion = pymysql.connect(
 
@@ -22,5 +23,32 @@ class Conexion:
             print("Conectado mi shark")
 
         except Exception as e:
-            print("Error "+ e)
+            print("Error " + e)
+
+        return cls._conexion
+
+    @classmethod
+    def listarEstudiantes(cls):
+            cursor = cls.obtenerConexion().cursor()
+            cursor.execute("SELECT * FROM estudiantes")
+            resultados = cursor.fetchall()
+            print(resultados)
+
+
+
+
+    @classmethod
+    def agregarEstudiante(cls, estu):
+            try:
+                cursor = cls.obtenerConexion().cursor()
+                sql = (f"INSERT INTO estudiantes ( idestudiantes, nombre, apellido, grado) VALUES ('4', 'mario', 'simon', '2')")
+                cursor.execute(sql)
+                cursor.connection.commit()
+                print("Registrado")
+
+            except Exception as ex:
+                print("Error: " + ex)
+
+
+
 
--- Main.py
+++ Main.py
@@ -1,26 +1,36 @@
 import Conexion
 from PyQt5 import QtWidgets,uic
+estu = (3,"maria","lopez","2")
 
-Conexion.Conexion.AgregarEstudiante()
+def guardarEstudiantes(estu):
+    Conexion.Conexion.agregarEstudiante(estu)
 
-def prueba_boton():
-    print("Sos tuani")
 
+
+#gGestiona todas las interacciones con la interfaz gráfica de usuario
 aplicacion = QtWidgets.QApplication([])
 
 #ventana del PyQt5
 ventana = uic.loadUi("Interfaz.ui")
+
+#Asignarle un titulo a la ventana
+ventana.setWindowTitle("Estudiantes")
 
 #Obtener texto de los Line edits
 h = ventana.Line_Id.text()
 print(h)
 
 #Interaccion con el boton
-ventana.BT_Guardar.clicked.connect(prueba_boton)
+ventana.BT_Guardar.clicked.connect(guardarEstudiantes)
+
 
 #Ejecutar ventana de PyQt5
-ventana.show()
-aplicacion.exec()
+if __name__ == '__main__':
+
+    #Muestra la ventana
+    ventana.show()
+    #Mantiene en ejecución la interfaz
+    aplicacion.exec()
 
 
 
