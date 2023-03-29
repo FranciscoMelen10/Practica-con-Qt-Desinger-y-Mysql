4b70835019551e9a79d22c96c3ec44e16a58cc3a
FranciscoMelen10 <francisco.melendez14454@est.uca.edu.ni>
2023-03-28 23:06:54 -0600

    Validación con ventana emegente (Solo BT_Guardar)

--- Interfaces/frm_Estudiantes.py
+++ Interfaces/frm_Estudiantes.py
@@ -1,31 +1,41 @@
 import sys
-
 from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QDialog, QMessageBox
 from PyQt5.uic import loadUi
-from Datos import dt_estudiante
-
-
-from Datos import dt_estudiante
-
 
 class Form_Estudiante(QtWidgets.QMainWindow):
-    def __init__(self, parent=None):
-        super(Form_Estudiante,self).__init__(parent)
+    def __init__(self):
+        self.indicador = 0
+
+        super(Form_Estudiante,self).__init__()
         loadUi(".\Interfaz.ui",self)
         self.BT_Guardar.clicked.connect(self.guardarEstudiantes)
 
     def guardarEstudiantes(self):
-        dt_estudiante.Dt_Estudiantes.agregarEstudiante(int(self.Line_Id.text()), self.Line_Nombre.text(),
-                                                       self.Line_Apellido.text(), int(self.Line_Grado.text()))
+        if not self.Line_Id.text() == "" and not self.Line_Nombre.text()== "" and not self.Line_Apellido.text()== "" and not self.Line_Grado.text()== "":
+            self.indicador = 1
+            #dt_estudiante.Dt_Estudiantes.agregarEstudiante(int(self.Line_Id.text()), self.Line_Nombre.text(),
+                                                       #self.Line_Apellido.text(), int(self.Line_Grado.text()))
+            self.mensaje(self.indicador)
+            self.indicador = 0
+            return self.indicador
 
-    def abrirVentana(self):
-        app = QtWidgets.QApplication(sys.argv)
-        aplicacion = Form_Estudiante()
-        aplicacion.show()
-        app.exec()
+        else:
+            self.mensaje(self.indicador)
+            indicador = 0
+            return indicador
 
-def main():
-    Form_Estudiante.abrirVentana("")
+    def mensaje(self,indicador):
+        if indicador == 1:
+            QMessageBox.about(self, "Registrado", "Datos registrados correctamente")
+
+        else:
+            QMessageBox.about(self, "Error", "Error de registros de datos")
 
 
-main()
+if __name__ == '__main__':
+    app = QtWidgets.QApplication(sys.argv)
+    aplicacion = Form_Estudiante()
+    aplicacion.show()
+    app.exec()
+
