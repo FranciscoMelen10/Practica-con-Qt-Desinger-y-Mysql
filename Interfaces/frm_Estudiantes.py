59fda6ce9f70228aa1b83d398204695ba636f16c
FranciscoMelen10 <francisco.melendez14454@est.uca.edu.ni>
2023-03-31 23:28:24 -0600

    YA ME FUNCIONA LA TABLAAAAAAAA

--- Interfaces/frm_Estudiantes.py
+++ Interfaces/frm_Estudiantes.py
@@ -1,18 +1,22 @@
 import sys
 from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import QDialog, QMessageBox
+from PyQt5.QtWidgets import QTableWidgetItem, QMessageBox
 from PyQt5.uic import loadUi
 
 from Datos import dt_estudiante
+from Entidades import estudiantes
 
 
 class Form_Estudiante(QtWidgets.QMainWindow):
     def __init__(self):
         self.indicador = 0
-
         super(Form_Estudiante,self).__init__()
         loadUi(".\Interfaz.ui",self)
+        self.llenarTabla(dt_estudiante.Dt_Estudiantes.listarEstudiantes())
+
+        #Acciones de los botones
         self.BT_Guardar.clicked.connect(self.guardarEstudiantes)
+
 
     def guardarEstudiantes(self):
         if not self.Line_Nombre.text()== "" and not self.Line_Apellido.text()== "" and not self.Line_Grado.text()== "":
@@ -33,6 +37,21 @@ class Form_Estudiante(QtWidgets.QMainWindow):
 
         else:
             QMessageBox.about(self, "Error", "Error de registros de datos")
+
+    def llenarTabla(self,datos):
+        i=len(datos)
+
+        self.TB_Estudiantes.setRowCount(i)
+        tablerow = 0
+
+        for row in datos:
+            print(row)
+            self.TB_Estudiantes.setItem(tablerow,0,QTableWidgetItem((row["nombre"])))
+            self.TB_Estudiantes.setItem(tablerow, 1, QTableWidgetItem((row["apellido"])))
+            self.TB_Estudiantes.setItem(tablerow, 2, QTableWidgetItem((row["grado"])))
+            tablerow = tablerow + 1
+
+
 
 
 if __name__ == '__main__':
